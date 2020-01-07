3ea90108fdc8fd701e6a17cd7a19b20811fc6d6f
SeoSang <ddrrpg@naver.com>
2020-01-07 12:17:32 +0900

    Updates

---
+++ precache-manifest.bfa69536db5a2f0db09337ae51934cb4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f848f6367de45330ba59bd9c12177707",
+    "url": "/my_app_2020/index.html"
+  },
+  {
+    "revision": "1fcdfebff62b73134515",
+    "url": "/my_app_2020/static/css/main.3e1325dc.chunk.css"
+  },
+  {
+    "revision": "15a5deec6558fb0b62ac",
+    "url": "/my_app_2020/static/js/2.cd831b6b.chunk.js"
+  },
+  {
+    "revision": "c6d01cb19213691bc81a274434d00fe7",
+    "url": "/my_app_2020/static/js/2.cd831b6b.chunk.js.LICENSE"
+  },
+  {
+    "revision": "1fcdfebff62b73134515",
+    "url": "/my_app_2020/static/js/main.fca4f0b2.chunk.js"
+  },
+  {
+    "revision": "7cd37134128b25256a7c",
+    "url": "/my_app_2020/static/js/runtime-main.070fe57d.js"
+  }
+]);
